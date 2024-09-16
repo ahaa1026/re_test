c6ceee7970c3341013d0cc1373c1ab342aaec1e4
ahaa1026 <952523908@qq.com>
2024-09-16 10:36:37 +0800

    uint8_t adc;

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -44,7 +44,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+uint8_t adc;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
